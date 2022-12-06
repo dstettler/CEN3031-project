2e89ad415ed410acbee19f2b0338b3a429cc392b
Johnny Huynh <johnnyhuynh561@gmail.com>
2022-12-06 06:18:12 -0500

    Added images into QLabels

--- source/relieffundsdialog.cpp
+++ source/relieffundsdialog.cpp
@@ -1,14 +1,28 @@
 #include "relieffundsdialog.h"
 #include "ui_relieffundsdialog.h"
+#include <QDesktopServices>
+#include <QUrl>
+#include <QPixmap>
 
 ReliefFundsDialog::ReliefFundsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ReliefFundsDialog)
 {
     ui->setupUi(this);
+
+    //Images we want in the relief funds page
+    QPixmap redCrossImage("/Users/johnnyhuynh/Downloads/RedCrossImage.png");
+    QPixmap directReliefImage("/Users/johnnyhuynh/Downloads/DirectReliefImage.png");
+    QPixmap disasterFundImage("/Users/johnnyhuynh/Downloads/DisasterFundImage.png");
+
+    //Setting the QLabels to the image
+    ui->RedCrossImage->setPixmap(redCrossImage);
+    ui->DirectReliefImage->setPixmap(directReliefImage);
+    ui->DisasterFundImage->setPixmap(disasterFundImage);
 }
 
 ReliefFundsDialog::~ReliefFundsDialog()
 {
     delete ui;
 }
+
